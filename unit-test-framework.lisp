38675096107560677e31216855379edea6357362
inaabadjieva <ina_abadjieva@yahoo.com>
2017-01-04 17:41:48 +0200

    simple unit test framework

---
+++ unit-test-framework.lisp
@@ -0,0 +1,44 @@
+(defvar *test-name* nil)
+
+(defmacro with-gensyms ((&rest names) &body body)
+  `(let ,(loop for n in names collect `(,n (gensym)))
+     ,@body))
+
+(defmacro deftest (name parameters &body body)
+  "Define a test function."
+  `(defun ,name ,parameters
+     (let ((*test-name* (append *test-name* (list ',name))))
+       ,@body)))
+
+(defmacro check (&body forms)
+  "Run each expression in 'forms' as a test case."
+  `(combine-results
+     ,@(loop for f in forms collect `(report-result ,f ',f))))
+
+(defmacro combine-results (&body forms)
+  "Combine the results (as booleans) of evaluating 'forms' in order."
+  (with-gensyms result)
+    `(let ((,result t))
+       ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
+       ,result)))
+
+(defun report-result (result form)
+  "Report the result of a single test case. Called by 'check'."
+  (format t "~:[FAIL~;pass~] ... ~a: ~a~%" result *test-name* form)
+  result)
+
+(deftest test-+ ()
+    (check
+      (= (+ 1 2) 3)
+      (= (+ 1 2 3) 6)
+      (= (+ -1 -3) -4))))
+
+(deftest test-* ()
+    (check
+      (= (* 2 2) 4)
+      (= (* 3 5) 15))))
+
+(deftest test-arithmetic ()
+  (combine-results
+    (test-+)
+    (test-*)))
